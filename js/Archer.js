8e0e2b802b946ba9797314405bb60aa612ff81b0
jwdallas <jondallas@gmail.com>
2012-12-13 09:28:55 -0800

    Create Archer.js
    dealer of hash, host, history and storage

---
+++ js/Archer.js
@@ -0,0 +1,9 @@
+/*! phiveleven.com/jquery.Archer */
+// dealer of hash, host, history and storage
+$.Archer = (function(){return;
+  $(document).on('load', function(){
+    recognize(location.hash, 'Welcome');
+    (location.hash)
+      .replace('',console.warn);
+  });
+})();
