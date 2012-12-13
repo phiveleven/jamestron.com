b8366798a356a51eea05e53cf66317661ba120ed
jwdallas <jondallas@gmail.com>
2012-12-13 09:30:45 -0800

    Create jquery-mangle.js

---
+++ js/jquery-mangle.js
@@ -0,0 +1,21 @@
+/*! phiveleven.com/jquery.mangle */
+// tamper with [data]
+$.fn.mangle = function(mangler){
+  console.debug('mangle: ', this, mangler);
+  // no [data]? do nothing
+  return this.filter('[data]').each(function(){
+    // holder of the datas, yo
+    var holder = $(this),
+        data = holder.data();
+    console.log(holder, data)
+      // store unmangled as JSON string in [data-src]
+    holder.find(':not([data-stash])')
+      // like fn.data but awesomer
+      .stash('data-unmangled', holder.data())
+      .attr('data-stash', true).end()
+      // replace data with mangled doppleganger
+      .removeData().data(
+          // apply lambda to data
+          $.each(holder.data(), mangler) );
+  }).end();
+};
